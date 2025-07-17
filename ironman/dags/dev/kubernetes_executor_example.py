bde3cd5b346b1b9c7c008a6b5575b77849f5504f
noppon <noppon.chantaban@gmail.com>
2025-07-17 10:05:24 +0700

    Set time to run every 1 minus

--- ironman/dags/dev/kubernetes_executor_example.py
+++ ironman/dags/dev/kubernetes_executor_example.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.utils.dates import days_ago
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
 default_args = {
     'owner': 'airflow',
@@ -9,27 +10,32 @@ default_args = {
 with DAG(
     dag_id='kubernetes_executor_example',
     default_args=default_args,
-    description='Simple DAG using KubernetesExecutor (no extra providers)',
-    schedule_interval=None,
-    start_date=days_ago(1),
+    description='DAG ที่ใช้ KubernetesPodOperator รันทุก 1 นาที',
+    schedule_interval='* * * * *',  # ทุก 1 นาที
+    start_date=datetime(2023, 1, 1),  # ตั้งเป็นอดีตแน่นอน
     catchup=False,
-    tags=['example', 'k8s'],
+    tags=['k8s', 'minute'],
 ) as dag:
 
     start = BashOperator(
         task_id='start',
-        bash_command='echo "Start KubernetesExecutor DAG"',
+        bash_command='echo "เริ่มต้น DAG ที่รันทุกนาที"',
     )
 
-    # ใช้ BashOperator แทน KubernetesPodOperator ชั่วคราว
-    run_in_k8s_simulated = BashOperator(
-        task_id='run_in_k8s_simulated',
-        bash_command='echo "Simulating task inside Kubernetes Pod (no KubernetesPodOperator)" && sleep 5',
+    run_in_k8s = KubernetesPodOperator(
+        task_id='run_in_kubernetes',
+        name='run-in-kubernetes',
+        namespace='airflow',
+        image='alpine:3.18',
+        cmds=["sh", "-c"],
+        arguments=["echo Hello from inside a Kubernetes Pod! && sleep 5"],
+        get_logs=True,
+        is_delete_operator_pod=True,
     )
 
     finish = BashOperator(
         task_id='finish',
-        bash_command='echo "Finished running DAG"',
+        bash_command='echo "DAG เสร็จสมบูรณ์"',
     )
 
-    start >> run_in_k8s_simulated >> finish
+    start >> run_in_k8s >> finish
