f1d10c7907d09b21697afc541e38afb4eabd3ffe
noppon <noppon.chantaban@gmail.com>
2025-07-23 15:09:23 +0700

    Improve Dags to support pods resource

--- ironman/dags/dev/kubernetes_pod_operator_example.py
+++ ironman/dags/dev/kubernetes_pod_operator_example.py
@@ -2,29 +2,45 @@ from airflow.operators.bash import BashOperator
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-import json
-import configparser
-import airflow.settings
-from airflow.models import DagModel
-import os
-from typing import Dict
-import importlib.util
-from airflow.operators.python import get_current_context
 from kubernetes.client import models as k8s_models
-from kubernetes.client import models as k8s
-from datetime import datetime, timedelta
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-import pytz
+from datetime import datetime
 
 default_args = {
     'owner': 'airflow',
 }
 
+# ปรับหน่วย memory จาก 5000M → 5000Mi เพื่อรองรับ Kubernetes
+resources_args = {
+    'request_memory': '256Mi',
+    'limit_memory': '5000Mi',
+    'request_cpu': '150m',
+    'limit_cpu': '500m',
+}
+
+labels_args = {
+    'app': 'airflow',
+    'type': 'batch',
+    'company': 'airflow',
+    'project': 'ironman'
+}
+
+# กำหนด resource requirements จากตัวแปร
+resource_requirements = k8s_models.V1ResourceRequirements(
+    requests={
+        "memory": resources_args["request_memory"],
+        "cpu": resources_args["request_cpu"],
+    },
+    limits={
+        "memory": resources_args["limit_memory"],
+        "cpu": resources_args["limit_cpu"],
+    },
+)
+
 with DAG(
     dag_id='kubernetes_pod_operator_example',
     default_args=default_args,
-    description='Simple DAG using kubernetesPodOperator',
+    description='Simple DAG using KubernetesPodOperator',
     schedule_interval=None,
     start_date=days_ago(1),
     catchup=False,
@@ -34,6 +50,8 @@ with DAG(
     start = BashOperator(
         task_id='start',
         bash_command='echo "Start KubernetesExecutor DAG"',
+        labels=labels_args,
+        container_resources=resource_requirements,
     )
 
     run_in_k8s = KubernetesPodOperator(
@@ -43,6 +61,8 @@ with DAG(
         image='alpine:3.18',
         cmds=["sh", "-c"],
         arguments=["echo Hello from inside a Kubernetes Pod! && sleep 5"],
+        labels=labels_args,
+        container_resources=resource_requirements,
         get_logs=True,
         is_delete_operator_pod=True,
     )
@@ -50,6 +70,8 @@ with DAG(
     finish = BashOperator(
         task_id='finish',
         bash_command='echo "DAG เสร็จสมบูรณ์"',
+        labels=labels_args,
+        container_resources=resource_requirements,
     )
 
     start >> run_in_k8s >> finish
